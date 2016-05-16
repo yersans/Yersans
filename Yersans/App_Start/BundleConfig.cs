d5ab469552640ff49747899a39a116c4dd6ffc10
yersans <yersans@hotmail.com>
2016-05-16 22:34:09 +0800

    datepicker

--- Yersans/App_Start/BundleConfig.cs
+++ Yersans/App_Start/BundleConfig.cs
@@ -26,6 +26,7 @@ namespace Yersans
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include("~/Scripts/jquery-ui-{version}.js"));
         }
     }
 }
