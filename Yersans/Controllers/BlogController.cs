841d93efab6396e43100ef75e2cfb1be7163aeb4
Leonardo <yersans@hotmail.com>
2016-11-11 18:01:38 +0800

    Ajax Delete Step 1.

--- Yersans/Controllers/BlogController.cs
+++ Yersans/Controllers/BlogController.cs
@@ -119,6 +119,17 @@ namespace Yersans.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult Movies()
+        {
+            var movies = new List<object>();
+
+            movies.Add(new { Title = "Ghostbusters", Genre = "Comedy", Year = 1984 });
+            movies.Add(new { Title = "Gone with Wind", Genre = "Drama", Year = 1939 });
+            movies.Add(new { Title = "Star Wars", Genre = "Science Fiction", Year = 1977 });
+
+            return Json(movies, JsonRequestBehavior.AllowGet);
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
